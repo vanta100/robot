53ed523feb7cf89123eed5b140f06f49730ad5a5
Van <van.ta@mavs.uta.edu>
2017-04-03 21:00:08 -0500

    Menu for robot_part finished

--- Duh_Robot_Proposal_Sprint_2.cpp
+++ Duh_Robot_Proposal_Sprint_2.cpp
@@ -33,10 +33,8 @@ class Locomotor : public Robot_part {
  private:
   double max_power;
  public:
-  Locomotor(string _name, int _model_number, double _cost, string _description) : Robot_part(_name,_model_number,_cost,_description) {}
-  void get_power(double p) {
-   max_power = p;
-  }
+  Locomotor(string _name, int _model_number, double _cost, string _description, double _max_power) : Robot_part(_name,_model_number,_cost,_description), max_power{_max_power} {}
+
 };
 
 class Torso : public Robot_part {
@@ -44,11 +42,8 @@ class Torso : public Robot_part {
   int battery_compartments;
   int max_arms;
  public:
-  Torso(string _name, int _model_number, double _cost, string _description) : Robot_part(_name,_model_number,_cost,_description) {}
-  void get_power_and_compartment(int bc, int ma) {
-   max_arms = ma;
-   battery_compartments = bc;
-  }
+  Torso(string _name, int _model_number, double _cost, string _description, int bat_comp, int _max_arms) : Robot_part(_name,_model_number,_cost,_description), battery_compartments{bat_comp}, max_arms{_max_arms} {}
+
 }; 
 
 class Battery : public Robot_part {
@@ -56,21 +51,16 @@ class Battery : public Robot_part {
   double power_available;
   double max_energy;
  public:
-  Battery(string _name, int _model_number, double _cost, string _description) : Robot_part(_name,_model_number,_cost,_description) {}
-  void get_powers(double p, double e) {
-   power_available = p;
-   max_energy = e;
-  }
+  Battery(string _name, int _model_number, double _cost, string _description ,double pow_avail, double max_ener) : Robot_part(_name,_model_number,_cost,_description), power_available{pow_avail}, max_energy{max_ener} {}
+
 };
 
 class Arm : public Robot_part {
  private:
   double max_power;
  public:
-  Arm(string _name, int _model_number, double _cost, string _description) : Robot_part(_name,_model_number,_cost,_description) {}
-  void get_power(double p) {
-   max_power = p;
-  }
+  Arm(string _name, int _model_number, double _cost, string _description, double max_pow) : Robot_part(_name,_model_number,_cost,_description), max_power{max_pow} {}
+
 };
 
 class Robot_model {
@@ -139,8 +129,8 @@ void Controller::execute() {
 
 void Controller::create_new_robot_parts() {
 string name, input;
-int model_number, get_out; //get_out is to break the loop
-double cost, power;
+int model_number, bat_compart, num_arms, get_out; //get_out is to break the loop
+double cost, power,max_energy;
 string description;
 
 cout << "\n\n(0) quit\n(1) head\n(2) torso\n(3) battery\n(4) locomotor\n(5) arm\nEnter one of the choices: ";
@@ -164,13 +154,14 @@ while(get_out != 0) {
    cin >> power;
    cin.ignore();
    
-   cost = 100;
+   cost = 1000;
    cout << "Cost for all custom heads is $" << cost << " NON-NEGOTIABLE\n";
    shop.create_new_robot_part(Head(name,model_number,cost,description,power));
    cout << "Successfully created " << name <<'\n';
-  }
+   break;
+   }
   case(2): {
-   cout << "Enter the name of the head: ";
+   cout << "Enter the name of the torso: ";
    getline(cin,name);
 
    cout << "Enter the desired model number: ";
@@ -180,47 +171,112 @@ while(get_out != 0) {
    cout << "Enter a description of the part: ";
    getline(cin,description);
 
-   cout << "How much power do you want your head to have?\n";
-   cin >> power;
+   cout << "How many arms do you want your robot to have?\n";
+   cin >> num_arms;
    cin.ignore();
-   
-   cost = 100;
-   cout << "Cost for all custom heads is $" << cost << " NON-NEGOTIABLE\n";
-   shop.create_new_robot_part(Head(name,model_number,cost,description,power));
+
+   cout << "How many battery compartments do you want your robot to have?\n";
+   cin >> bat_compart;
+   cin.ignore();
+
+   cost = 2000;
+   cout << "Cost for all custom torsos is $" << cost << " NON-NEGOTIABLE\n";
+   shop.create_new_robot_part(Torso(name,model_number,cost,description,bat_compart,num_arms));
    cout << "Successfully created " << name <<'\n';
-  
+   break;
   }
   case(3): {
-   
+   cout << "Enter the name of the battery: ";
+   getline(cin,name);
+
+   cout << "Enter the desired model number: ";
+   cin >> model_number;
+   cin.ignore();
+
+   cout << "Enter a description of the part: ";
+   getline(cin,description);
+
+   cout << "How much power do you want your battery to have?\n";
+   cin >> power;
+   cin.ignore();
+
+   cout << "What is the max energy you want to put on your battery?\n";
+   cin >> max_energy;
+   cin.ignore();
+
+   cost = 1000;
+   cout << "Cost for all custom batteries is $" << cost << " NON-NEGOTIABLE\n";
+   shop.create_new_robot_part(Battery(name,model_number,cost,description,power,max_energy));
+   cout << "Successfully created " << name <<'\n';
+   break;
   }
   case(4): {
+   cout << "Enter the name of the locomotor: ";
+   getline(cin,name);
+
+   cout << "Enter the desired model number: ";
+   cin >> model_number;
+   cin.ignore();
+
+   cout << "Enter a description of the part: ";
+   getline(cin,description);
+
+   cout << "What is the max power you want to put on your locomotor?\n";
+   cin >> power;
+   cin.ignore();
+
+   cost = 3000;
+   cout << "Cost for all custom locomotors is $" << cost << " NON-NEGOTIABLE\n";
+   shop.create_new_robot_part(Locomotor(name,model_number,cost,description,power));
+   cout << "Successfully created " << name <<'\n';
+   break;
    
   }
   case(5): {
+   cout << "Enter the name of the arms: ";
+   getline(cin,name);
 
+   cout << "Enter the desired model number: ";
+   cin >> model_number;
+   cin.ignore();
+
+   cout << "Enter a description of the part: ";
+   getline(cin,description);
+
+   cout << "What is the max power you want to put on your arms?\n";
+   cin >> power;
+   cin.ignore();
+
+   cost = 1000;
+   cout << "Cost for all custom arms is $" << cost << "each NON-NEGOTIABLE\n";
+   shop.create_new_robot_part(Locomotor(name,model_number,cost,description,power));
+   cout << "Successfully created " << name <<'\n';
+   break;
+   
   }
   default: {
    cout<<"\nWould you like to continue adding parts?\n\n(0) quit\n(1) head\n(2) torso\n(3) battery\n(4) locomotor\n(5) arm\nEnter one of the choices: ";
    cin >> get_out;
-   cin.ignore();
   }
  }
+    cout<<"\nWould you like to continue adding parts?\n\n(0) quit\n(1) head\n(2) torso\n(3) battery\n(4) locomotor\n(5) arm\nEnter one of the choices: ";
+   cin >> get_out;
 }
 
 
-shop.create_new_robot_part(Torso("Torso",1,1.00,"lol"));
-shop.create_new_robot_part(Battery("Battery",1,1.00,"lol"));
-shop.create_new_robot_part(Locomotor("Locomotor",1,1.00,"lol"));
-shop.create_new_robot_part(Arm("Arm",1,1.00,"lol"));
+shop.create_new_robot_part(Torso("Torso",1,1.00,"lol",1,1));
+shop.create_new_robot_part(Battery("Battery",1,1.00,"lol",1.0,1.0));
+shop.create_new_robot_part(Locomotor("Locomotor",1,1.00,"lol",1.0));
+shop.create_new_robot_part(Arm("Arm",1,1.00,"lol",1.0));
 
 }
 
 void Controller::create_new_robot_model() {
 Head head("Head",1,1.00,"lol",1.0);
-Torso torso("Torso",1,1.00,"lol");
-Battery battery("Battery",1,1.00,"lol");
-Locomotor locomotor("Locomotor",1,1.00,"lol");
-Arm arm("Arm",1,1.00,"lol");
+Torso torso("Torso",1,1.00,"lol",1,1);
+Battery battery("Battery",1,1.00,"lol",1.0,1.0);
+Locomotor locomotor("Locomotor",1,1.00,"lol",1.0);
+Arm arm("Arm",1,1.00,"lol",1.0);
 Robot_model robot_model(head, torso, battery, locomotor, arm);
 shop.create_new_robot_model(robot_model);
 }
